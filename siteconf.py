47e1c6a0c5345ca076d0320b130380f5a7ca9428
Nachiket Kapre <nachiket@ieee.org>
2016-08-05 22:33:28 +0800

    adding siteconf for Altera

---
+++ siteconf.py
@@ -0,0 +1,9 @@
+CL_TRACE = False
+CL_ENABLE_GL = False
+CL_USE_SHIPPED_EXT = True
+CL_PRETEND_VERSION = '1.1'
+CL_INC_DIR = []
+CL_LIB_DIR = ['/opt/altera/16.0/hld/board/terasic/de5net/linux64/lib', '/opt/altera/16.0/hld/host/linux64/lib']
+CL_LIBNAME = ['alteracl', 'alterahalmmd', 'altera_apb_14_0_mmd']
+CXXFLAGS = ['-std=c++0x']
+LDFLAGS = ['-Wl,--no-as-needed','-Wl,--rpath,/opt/altera/16.0/hld/board/terasic/de5net/linux64/lib','-Wl,--rpath,/opt/altera/16.0/hld/host/linux64/lib']
